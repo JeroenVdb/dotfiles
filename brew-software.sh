a519ca1c371e3477a5b08b5793cfa84b84016645
Jeroen Van den Berghe <vandenberghe.jeroen@gmail.com>
2018-01-10 18:35:51 +0100

    Update brew-software.sh

--- brew-software.sh
+++ brew-software.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
+# browsers
+brew cask install google-chrome
+
+# media
 brew cask install spotify
+
+# other
+brew cask install 1password
+
+# @HOME
+brew cask install plex-media-server
+brew cask install transmission
+
+# @WORK
+brew cask install hipchat
+brew cask install postman
