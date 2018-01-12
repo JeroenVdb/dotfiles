def637c8e464ffb72486b4d9418b08c4176ababa
Jeroen Van den Berghe <Jeroen.Van.den.Berghe@persgroep.be>
2018-01-12 15:55:49 +0100

    iterm docs

--- brew-development.sh
+++ brew-development.sh
@@ -5,3 +5,4 @@ brew install nvm
 # Use iTerm
 # Docs: http://stratus3d.com/blog/2015/02/28/sync-iterm2-profile-with-dotfiles-repository/
 # Store config files in dotfiles repo + use macos defaults to point to that directory
+# hyper-snazzy -> iterm2-snazzy: https://github.com/sindresorhus/iterm2-snazzy
