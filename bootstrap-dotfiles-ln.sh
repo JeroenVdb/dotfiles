37f8386ccb8b694433f7257f68583e0afd7835ed
Jeroen Van den Berghe <vandenberghe.jeroen@gmail.com>
2018-04-14 12:20:42 +0200

    Remove existing directories before symlinking

--- bootstrap-dotfiles-ln.sh
+++ bootstrap-dotfiles-ln.sh
@@ -2,21 +2,52 @@
 
 echo "Create symlinks: start"
 
+rm -rf ~/.aliases
 ln -s -v ~/Projects/dotfiles/.aliases ~/.aliases
+
+rm -rf ~/.atom
 ln -s -v ~/Projects/dotfiles/.atom/ ~/.atom
+
+rm -rf ~/.vscode
 ln -s -v ~/Projects/dotfiles/.vscode/ ~/.vscode
+
+rm -rf ~/Library/Application\ Support/Code/User
 ln -s -v ~/Projects/dotfiles/VSCodeUser ~/Library/Application\ Support/Code/User
+
+rm -rf ~/.bash_profile
 ln -s -v ~/Projects/dotfiles/.bash_profile ~/.bash_profile
+
+rm -rf ~/.bashrc
 ln -s -v ~/Projects/dotfiles/.bashrc ~/.bashrc
+
+rm -rf ~/.exports
 ln -s -v ~/Projects/dotfiles/.exports ~/.exports
+
+rm -rf ~/.functions
 ln -s -v ~/Projects/dotfiles/.functions ~/.functions
+
+rm -rf ~/.git
 ln -s -v ~/Projects/dotfiles/.git/ ~/.git
+
+rm -rf ~/.gitconfig
 ln -s -v ~/Projects/dotfiles/.gitconfig ~/.gitconfig
+
+rm -rf ~/.gitignore
 ln -s -v ~/Projects/dotfiles/.gitignore ~/.gitignore
+
+rm -rf ~/.hyper.js
 ln -s -v ~/Projects/dotfiles/.hyper.js ~/.hyper.js
+
+rm -rf ~/.inputrc
 ln -s -v ~/Projects/dotfiles/.inputrc ~/.inputrc
+
+rm -rf ~/.macos
 ln -s -v ~/Projects/dotfiles/.macos ~/.macos
+
+rm -rf ~/.osx
 ln -s -v ~/Projects/dotfiles/.osx ~/.osx
+
+rm -rf ~/.zshrc
 ln -s -v ~/Projects/dotfiles/.zshrc ~/.zshrc
 
 echo "Create symlinks: done"
